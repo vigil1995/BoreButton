88feb2cb41b7fd5e397df735b87d0914a484633e
vigil joshy <vigil.joshy1@bp.com>
2020-08-16 11:42:28 +0530

    2 nd

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {WelcomeScreen,TaskScreen} from './app/screens'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        
+      </Stack.Navigator>
+    
+
+    </NavigationContainer>
   );
 }
 
